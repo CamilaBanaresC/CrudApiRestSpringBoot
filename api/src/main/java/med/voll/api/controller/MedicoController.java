6e95622e3b9348a2110b8cde9856f924f0a9c24a
Camila Bañares <camiladanielabanarescarrasco@gmail.com>
2024-06-14 23:44:44 -0400

    Se crea DeleteMapping para eliminar registro de la bbbdd

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,9 +45,18 @@ public class MedicoController {
 	
 	
 	@PutMapping
-	@Transactional
+	@Transactional //funciona para que se realice un comit en la bbdd
 	public void actualizarMedico(@RequestBody @Valid RequestActualizarRegistroMedico requestActualizarRegistroMedico) {
 		Medico medico = medicoRepository.getReferenceById(requestActualizarRegistroMedico.id());
 		medico.actualizarDatos(requestActualizarRegistroMedico);
 	}
+	
+	@DeleteMapping("/{id}") //Valor enviado desde el path en insomnia o postman
+	@Transactional
+	public void eliminarMecido(@PathVariable  Long id) {
+	Medico medico = medicoRepository.getReferenceById(id);
+	medicoRepository.delete(medico);
+	}
+	
+	
 }
